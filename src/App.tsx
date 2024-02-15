f5e62c9f299b8d3561f9f8f232ced1b6dc1774e2
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2024-02-15 23:39:17 +0300

    ADD basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 import './globals.css';
 
+import SigninForm from '_auth/forms/SigninForm';
+import SignupForm from '_auth/forms/SignupForm';
+import AuthLayout from '_auth/AuthLayout';
+import { Home } from '_root/pages';
+import RootLayout from '_root/RootLayout';
+
 const App = () => {
 	return (
 		<Routes>
-			<Route path='' element={<h1>Hello World</h1>} />
+			{/* Public routes */}
+			<Route element={<AuthLayout />}>
+				<Route path='singin' element={<SigninForm />} />
+				<Route path='signup' element={<SignupForm />} />
+			</Route>
+
+			{/* Private routes */}
+			<Route element={<RootLayout />}>
+				<Route index element={<Home />} />
+			</Route>
 		</Routes>
 	);
 };
